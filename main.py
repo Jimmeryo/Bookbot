ef0cb76fe6b3ecff68f587d41f2b0d86fce1bf8a
Vladyslav Marchuk <augustime634@gmail.com>
2025-03-11 13:36:53 +0200

    nothing new lol

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+
+def txt_read_and_write ():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    s_text = txt_read_and_write()
+    print(len(txt_read_and_write().split()),"words found in the document")
+
+if __name__ == "__main__":
+    main()
